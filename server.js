d2369b4ad367e3803116f1c93256cc51386941f6
Cristian Angulo <paul.angulo.anyosa@gmail.com>
2020-05-18 20:26:56 -0500

    Sección 7: Webserver - HTTP - EXPRESS - HBS

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require( 'express' );
 const app = express();
-const hbs = require( 'hbs' );
 
+const hbs = require( 'hbs' );
+require( './hbs/helpers' );
+
+const port = process.env.PORT || 3000;
 
 app.use( express.static( __dirname + '/public' ));
 
 // Express HBS Engine
 hbs.registerPartials( __dirname + '/views/partials' );
 app.set('view engine', 'hbs');
-
-// helpers
-
-hbs.registerHelper( 'getYear', () => {
-    return new Date().getFullYear();
-});
 
 app.get( '/', ( req, res ) => {
 
@@ -29,6 +26,6 @@ app.get( '/about', ( req, res ) => {
 
 });
 
-app.listen( 3000 , () => {
-    console.log( 'Escuchando peticiones en el puerto 3000' );
+app.listen( port , () => {
+    console.log( `Escuchando peticiones en el puerto ${ port }` );
 });
